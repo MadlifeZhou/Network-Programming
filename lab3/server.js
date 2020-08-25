5a93d168a0c39c88cdf38ca1a0408b8cbb4c86b9
MadlifeZhou <530518650@qq.com>
2020-08-25 15:55:50 +0800

    update data from server

--- lab3/server.js
+++ lab3/server.js
@@ -1,52 +1,43 @@
-// // 导入WebSocket模块:
-// const WebSocket = require('ws');
-//
-// // 实例化:
-// const wss = new WebSocket.Server({
-//     //端口号
-//     port: 3000
-// });
-//
-// console.log("setup server...");
-//
-// wss.on('connection', function (ws) {
-//     console.log(`[SERVER] connection()`);
-//     ws.on('message', function (message) {
-//         let json_message = JSON.parse(message);
-//         console.log(json_message.username)
-//         console.log(`[SERVER] Received: ${message}`);
-//         let message_string = JSON.stringify(json_message);
-//         ws.send(message_string, (err) => {
-//             if (err) {
-//                 console.log(`[SERVER] error: ${err}`);
-//             }
-//         });
-//     })
-// });
-
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express();
+const querystring = require('querystring');
 const port = 3000;
 
+// 创建 application/x-www-form-urlencoded 编码解析
+let urlencodedParser = bodyParser.urlencoded({extended: false})
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 let data = [
-    {"username":"zhou","comment":"ITMO is a good University. It`s always my dream to study in ITMO"},
-    {"username":"zhou2","comment":"HDU is a good University. It`s always my dream to study in HDU"}
+    {"username": "zhou", "comment": "ITMO is a good University. It`s always my dream to study in ITMO"},
+    {"username": "zhou2", "comment": "HDU is a good University. It`s always my dream to study in HDU"}
 ]
 
-app.get('/data',(request,response)=>{
+app.post('/', (request, response) => {
+
+    console.log(request.body)
+    response.end("hello")
+    response.send(data)
+});
+
+app.get('/', (request, response) => {
+    response.setHeader("Access-Control-Allow-Origin", "*");
     response.send(data);
 });
 
-app.get('/',(request,response)=>{
-   data.push(request.data)
-   response.send(data);
-});
-
-app.listen(port,(err)=>{
-   if (err){
-       return console.log('something bad happened',err)
-   }
-   console.log(`server is listening on ${port}`)
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
 });
 
 
